da92fd01192bc9652b4fa600efa3d5ef4a9bc4d7
marilina-a <marilina_1993@hotmail.com>
2020-06-28 19:45:49 -0300

    Se agrega el servicio MockArticulosFamilias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ArticulosFamiliasComponent } from './articulos-familias/articulos-familias.component';
+import { MockArticulosFamiliasService } from './services/mock-articulos-familias.service';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, InicioComponent, ArticulosFamiliasComponent],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [MockArticulosFamiliasService]
 })
 export class AppModule { }
--- src/app/articulos-familias/articulos-familias.component.ts
+++ src/app/articulos-familias/articulos-familias.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ArticuloFamilia, ArticulosFamilias } from '../models/articulo-familia';
-
+import { Component, OnInit } from "@angular/core";
+import { ArticuloFamilia } from "../models/articulo-familia";
+import { MockArticulosFamiliasService } from "../services/mock-articulos-familias.service";
+ 
 @Component({
-  selector: 'app-articulos-familias',
-  templateUrl: './articulos-familias.component.html',
-  styleUrls: ['./articulos-familias.component.css']
+  selector: "app-articulos-familias",
+  templateUrl: "./articulos-familias.component.html",
+  styleUrls: ["./articulos-familias.component.css"]
 })
 export class ArticulosFamiliasComponent implements OnInit {
-   Items = ArticulosFamilias;
-  Titulo = "Articulos Familias"
-  constructor() { }
-
+  Titulo = "Articulos Familias";
+  Items: ArticuloFamilia[] = [];
+ 
+  constructor(
+    private articulosFamiliasService:  MockArticulosFamiliasService
+  ){}
+ 
   ngOnInit() {
+    this.GetFamiliasArticulos();
   }
-
+ 
+  GetFamiliasArticulos() {
+    this.articulosFamiliasService.get()
+    .subscribe((res:ArticuloFamilia[]) => {
+      this.Items = res;
+    });
+  }
 }
